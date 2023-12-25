befbf03621b2c6c5d734d2e757d307e55fc0c397
arbaaznitz <154117381+arbaaznitz@users.noreply.github.com>
2023-12-25 15:17:17 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,18 @@
+public class Introduction {
+    public static void main(String[] args) {
+        // Replace these values with your actual information
+        String name = "Arbaj Shaikh";
+        String contactNumber = "Your Contact Number";
+        String address = "Your Address";
+        String phoneNumber = "Your Phone Number";
+        String profession = "Your Profession";
+
+        // Print the introduction
+        System.out.println("Introduction:");
+        System.out.println("Name: " + name);
+        System.out.println("Contact Number: " + contactNumber);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Profession: " + profession);
+    }
+}
